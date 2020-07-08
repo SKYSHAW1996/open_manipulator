db75294a310be4ce1df1527dc36fff57cea24f09
Will Son <willson@robotis.com>
2020-07-08 16:54:56 +0900

    add git install

--- install_ros_dashing.sh
+++ install_ros_dashing.sh
@@ -23,7 +23,7 @@ sudo apt update && sudo apt install -y curl gnupg2 lsb-release
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 sudo sh -c 'echo "deb [arch=amd64,arm64] http://packages.ros.org/ros2/ubuntu `lsb_release -cs` main" > /etc/apt/sources.list.d/ros2-latest.list'
 sudo apt update && sudo apt install -y ros-dashing-desktop
-sudo apt install -y python3-argcomplete python3-colcon-common-extensions python3-vcstool
+sudo apt install -y python3-argcomplete python3-colcon-common-extensions python3-vcstool git
 
 echo "[Make the robotis workspace folder and test colcon build]"
 source /opt/ros/$name_ros_version/setup.sh
